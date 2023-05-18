d656acf32829ade351789afbe74ba3cbdc60ea3c
Jeremy Whiting <jwhiting@redhat.com>
2023-05-18 16:25:51 +0100

    Ensure the project being worked on is active early in the script. Declare the secret be created in the namespace.

--- eapxp4/install.sh
+++ eapxp4/install.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 # source the env config, create this if it doesn't exist already
 . properties.sh
 
+oc project ${NAMESPACE}
+
 # login to openshift
 oc login --token=${AUTH_TOKEN} --server=${OPENSHIFT_URL}
 
 # create pull secret
-oc create secret docker-registry eap-pull-secret --docker-server=${DOCKER_SERVER} --docker-username=${USERNAME} --docker-password=${PASSWORD} --docker-email=${EMAIL} || true
-
-oc project ${NAMESPACE}
+oc -n ${NAMESPACE} create secret docker-registry eap-pull-secret --docker-server=${DOCKER_SERVER} --docker-username=${USERNAME} --docker-password=${PASSWORD} --docker-email=${EMAIL} || true
 
 # helm install the app
 helm install ${APP_NAME} -f values.yaml -f app.yaml -n ${NAMESPACE} --repo https://jbossas.github.io/eap-charts eap-xp4
